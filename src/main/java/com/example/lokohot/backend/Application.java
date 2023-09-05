d1661b3cd263d7a74e3e138076e524c981ce1bbc
IJOkl <klainerdo@gmail.com>
2023-09-05 20:01:01 +0300

    Initial commit

---
+++ src/main/java/com/example/lokohot/backend/Application.java
@@ -0,0 +1,25 @@
+package com.example.lokohot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+	@RequestMapping("/")
+	public String hello() {
+		return String.format("Hello %s!", "World");
+	}
+
+
+}
